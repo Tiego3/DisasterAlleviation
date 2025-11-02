421ca0858beb388c3a5bd4890a85de577dfa6283
Tiego3 <tiego3m@gmail.com>
2025-11-02 08:51:53 +0200

    Reorganized the solution structure by moving the main project from a subfolder to the root directory and fixing all related project references and build issues.

--- Migrations/20241001192102_updateprof.cs
+++ Migrations/20241001192102_updateprof.cs
@@ -5,7 +5,7 @@
 namespace DisasterAlleviation.Migrations
 {
     /// <inheritdoc />
-    public partial class updateprof : Migration
+    public partial class updateProf : Migration
     {
         /// <inheritdoc />
         protected override void Up(MigrationBuilder migrationBuilder)
